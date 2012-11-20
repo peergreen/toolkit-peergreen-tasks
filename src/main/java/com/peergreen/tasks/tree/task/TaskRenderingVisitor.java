e2dbcc4ee1696038cdfa291fdcc770dffac0e95b
Guillaume Sauthier <guillaume.sauthier@peergreen.com>
2012-11-20 11:18:03 +0100

    Prettify the printed tree

--- src/main/java/com/peergreen/tasks/tree/task/TaskRenderingVisitor.java
+++ src/main/java/com/peergreen/tasks/tree/task/TaskRenderingVisitor.java
@@ -5,6 +5,9 @@ import com.peergreen.tasks.tree.Node;
 import com.peergreen.tasks.tree.NodeVisitor;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,11 +15,31 @@ import java.io.PrintStream;
  * Date: 15/11/12
  * Time: 11:12
  * To change this template use File | Settings | File Templates.
+ * Sample output:
+ * <pre>
+ * Pipeline #master
+ * |-- Pipeline #p1
+ * |   |-- UnitOfWork #1
+ * |   `-- UnitOfWork #2
+ * |-- UnitOfWork #3
+ * `-- Pipeline #p2
+ *     |-- UnitOfWork #4
+ *     `-- UnitOfWork #5
+ * </pre>
  */
 public class TaskRenderingVisitor implements NodeVisitor<Task> {
 
-    public static final String SPACES = "  ";
-    private PrintStream stream;
+    private enum Element {
+        TEE("|-- "), BAR("|   "), LAST("`-- "), BLANK("    ");
+
+        private final String value;
+
+        Element(String value) {
+            this.value = value;
+        }
+    }
+
+    private PrintStream stream ;
 
     public TaskRenderingVisitor() {
         this(System.out);
@@ -28,13 +51,39 @@ public class TaskRenderingVisitor implements NodeVisitor<Task> {
 
     @Override
     public void visit(Node<Task> node) {
-        int depth = getDepth(node);
 
-        // Print increment
-        for (int i = 0; i < depth; i++) {
-            stream.print(SPACES);
+        List<Element> prefix = new ArrayList<Element>();
+        if (node.getParent() != null) {
+            // we're visiting an item with parent
+
+            // handle last element
+            if (isLastChild(node)) {
+                prefix.add(0, Element.LAST);
+            } else {
+                prefix.add(0, Element.TEE);
+            }
+
+            // handle other elements
+            Node<Task> current = node.getParent();
+            while (current.getParent() != null) {
+
+                if (isLastChild(current)) {
+                    prefix.add(0, Element.BLANK);
+                } else {
+                    prefix.add(0, Element.BAR);
+                }
+
+                // Move cursor
+                current = current.getParent();
+            }
+
+        } // else no parent (thus no/empty prefix)
+
+        for (Element element : prefix) {
+            stream.print(element.value);
         }
 
+        // Print Task info
         Task task = node.getData();
         stream.printf("%s [%s, %S]%n",
                 task.getClass().getSimpleName(),
@@ -42,14 +91,18 @@ public class TaskRenderingVisitor implements NodeVisitor<Task> {
                 task.getState());
     }
 
-    private int getDepth(Node<?> node) {
-        int depth = 0;
-        Node<?> current = node;
-        while(current.getParent() != null) {
-            depth++;
-            current = current.getParent();
+    private boolean isLastChild(Node<?> node) {
+        Node<?> parent = node.getParent();
+        Iterator<? extends Node<?>> i = parent.getChildren().iterator();
+
+        // Consume the Iterator until we find the current Node
+        Node<?> child = i.next();
+        while (!node.equals(child)) {
+            child = i.next();
         }
 
-        return depth;
+        // It is the last child if this is the last item of the Iterator
+        return !i.hasNext();
     }
+
 }
---
+++ src/test/java/com/peergreen/tasks/tree/task/TaskRenderingVisitorTestCase.java
@@ -0,0 +1,116 @@
+package com.peergreen.tasks.tree.task;
+
+import com.peergreen.tasks.model.Pipeline;
+import com.peergreen.tasks.model.Task;
+import com.peergreen.tasks.model.UnitOfWork;
+import com.peergreen.tasks.model.job.EmptyJob;
+import com.peergreen.tasks.tree.Node;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guillaume
+ * Date: 20/11/12
+ * Time: 10:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class TaskRenderingVisitorTestCase {
+
+    private ByteArrayOutputStream baos;
+    private TaskRenderingVisitor visitor;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        baos = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(baos);
+        visitor = new TaskRenderingVisitor(stream);
+    }
+
+    @Test
+    public void testSingleNodeRendering() throws Exception {
+        UnitOfWork unitOfWork = new UnitOfWork(new EmptyJob(), "uow");
+        Node<Task> node = new Node<Task>(new TaskNodeAdapter(), unitOfWork);
+
+        node.walk(visitor);
+
+        Assert.assertEquals(baos.toString(), "UnitOfWork [uow, WAITING]\n");
+    }
+
+    @Test
+    public void testPipelineNodeRendering() throws Exception {
+        Pipeline master = new Pipeline("master");
+        UnitOfWork unitOfWork = new UnitOfWork(new EmptyJob(), "uow");
+        master.add(unitOfWork);
+        Node<Task> node = new Node<Task>(new TaskNodeAdapter(), master);
+
+        node.walk(visitor);
+
+        String expected = "Pipeline [master, WAITING]\n" +
+                "`-- UnitOfWork [uow, WAITING]\n";
+        Assert.assertEquals(baos.toString(), expected);
+    }
+
+    @Test
+    public void testPipelineNodeRenderingWithTwoChildren() throws Exception {
+        Pipeline master = new Pipeline("master");
+        UnitOfWork unitOfWork = new UnitOfWork(new EmptyJob(), "uow");
+        UnitOfWork unitOfWork2 = new UnitOfWork(new EmptyJob(), "uow2");
+        master.add(unitOfWork, unitOfWork2);
+        Node<Task> node = new Node<Task>(new TaskNodeAdapter(), master);
+
+        node.walk(visitor);
+
+        String expected = "Pipeline [master, WAITING]\n" +
+                "|-- UnitOfWork [uow, WAITING]\n" +
+                "`-- UnitOfWork [uow2, WAITING]\n";
+        Assert.assertEquals(baos.toString(), expected);
+    }
+
+    @Test
+    public void testNodeRenderingWithBlankElement() throws Exception {
+        Pipeline master = new Pipeline("master");
+        UnitOfWork unitOfWork = new UnitOfWork(new EmptyJob(), "uow");
+        master.add(unitOfWork);
+
+        Pipeline sub = new Pipeline("sub");
+        sub.add(new UnitOfWork(new EmptyJob(), "task"));
+        master.add(sub);
+        Node<Task> node = new Node<Task>(new TaskNodeAdapter(), master);
+
+        node.walk(visitor);
+
+        String expected = "Pipeline [master, WAITING]\n" +
+                "|-- UnitOfWork [uow, WAITING]\n" +
+                "`-- Pipeline [sub, WAITING]\n" +
+                "    `-- UnitOfWork [task, WAITING]\n";
+        Assert.assertEquals(baos.toString(), expected);
+    }
+
+    @Test
+    public void testNodeRenderingWithBarElement() throws Exception {
+        Pipeline master = new Pipeline("master");
+        Pipeline pipeline = new Pipeline("pipeline");
+        master.add(pipeline);
+        UnitOfWork unitOfWork = new UnitOfWork(new EmptyJob(), "uow");
+        pipeline.add(unitOfWork);
+
+        Pipeline sub = new Pipeline("sub");
+        sub.add(new UnitOfWork(new EmptyJob(), "task"));
+        master.add(sub);
+        Node<Task> node = new Node<Task>(new TaskNodeAdapter(), master);
+
+        node.walk(visitor);
+
+        String expected = "Pipeline [master, WAITING]\n" +
+                "|-- Pipeline [pipeline, WAITING]\n" +
+                "|   `-- UnitOfWork [uow, WAITING]\n" +
+                "`-- Pipeline [sub, WAITING]\n" +
+                "    `-- UnitOfWork [task, WAITING]\n";
+        Assert.assertEquals(baos.toString(), expected);
+    }
+}
